3e002842e8e6d1beec2b04cdc52fb54c232b189e
MrFrosty1234 <mitrusinmaksim@gmail.com>
2023-10-25 19:51:12 +0300

    @oaleksandr pls check pipeline

--- Team17517/src/main/java/org/woen/team17517/Robot/OpenCV/PipeLine.java
+++ Team17517/src/main/java/org/woen/team17517/Robot/OpenCV/PipeLine.java
@@ -4,19 +4,15 @@ package org.woen.team17517.Robot.OpenCV;
 import static org.opencv.core.Core.*;
 import static org.opencv.imgproc.Imgproc.*;
 
-import android.graphics.Camera;
 import android.graphics.Canvas;
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.woen.team17517.Robot.UltRobot;
 
 public class PipeLine implements VisionProcessor {
     double x = 640;
@@ -27,13 +23,13 @@ public class PipeLine implements VisionProcessor {
     double r2 = 60;
     double g2 = 129;
     double b2 = 296;
-    Mat img_range_red;
-    Mat img_range_blue;
-    UltRobot robot;
+    Mat img_range_red = new Mat();
+    Mat img_range_blue = new Mat();
+    //UltRobot robot;
     double centerOfRectX = 0;
     double centerOfRectY = 0;
     public int pos = 0;
-
+    public boolean team = true;
 
     public void init(int width, int height, CameraCalibration calibration) {
 
@@ -41,23 +37,31 @@ public class PipeLine implements VisionProcessor {
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+        //
+        cvtColor(frame, frame, COLOR_RGB2HSV);
         resize(frame, frame, new Size(x, y));
 
         blur(frame, frame, new Size(10, 10));
 
-        inRange(frame, new Scalar(0, 0, 0), new Scalar(50, 83, 221), img_range_red);
-        inRange(frame, new Scalar(240, 0, 0), new Scalar(90, 82, 240), img_range_blue);
+        inRange(frame, new Scalar(0, 0, 0), new Scalar(221, 50, 83), img_range_red);
+        inRange(frame, new Scalar(240, 82, 90), new Scalar(0, 0, 240), img_range_blue);
+
+        if (mean(frame).val[0] > 10) {
+            team = true;
+        } else {
+            team = false;
+        }
+
 
         Rect boundingRect = boundingRect(frame);
 
         centerOfRectX = boundingRect.x + boundingRect.width / 2;
         centerOfRectY = boundingRect.y + boundingRect.height / 2;
 
-        if (centerOfRectX < 160 && centerOfRectX > 0) {
+        if (centerOfRectX < 200 && centerOfRectX > 0) {
             pos = 1;
         }
-        if (centerOfRectX < 320 && centerOfRectX > 160) {
+        if (centerOfRectX < 360 && centerOfRectX > 160) {
             pos = 2;
         }
         if (centerOfRectX < 480 && centerOfRectX > 320) {
