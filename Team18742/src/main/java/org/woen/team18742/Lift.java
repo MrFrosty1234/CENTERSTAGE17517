d73245b24fffd44f0f6c3a56e493d01728c8b102
dukar <dukarevichroman@gmail.com>
2023-10-27 21:00:25 +0300

    ЛИФТ

--- Team18742/src/main/java/org/woen/team18742/Lift.java
+++ Team18742/src/main/java/org/woen/team18742/Lift.java
@@ -10,24 +10,25 @@ public class Lift {
     private DcMotor _grabberDrive = null;
     private DcMotor _liftM1 = null;
     private DcMotor _liftM2 = null;
-    private Servo _servoLift1, _servoLift2;
+    public Servo _servoLift1, _servoLift2;
+
 
     void moveLift(double distance) {
-        _liftM1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        _liftM1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        double errold;
-        double kp = 1;
-        double kd = 1;
-        double err = distance - _liftM1.getCurrentPosition() / 480 * 360;
-        errold = err;
-        while (abs(err) > 2 && _collector.CommandCode.opModeIsActive()) {
-            err = distance - _liftM1.getCurrentPosition() / 480 * 360;
-            double u = (err * kp) + (err - errold) * kd;
-            _liftM1.setPower(u);
-            _liftM2.setPower(u);
-        }
-        _liftM1.setPower(0);
-        _liftM2.setPower(0);
+                _liftM1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                _liftM1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                double errold;
+                double kp = 1;
+                double kd = 1;
+                double err = distance - _liftM1.getCurrentPosition() / 480 * 360;
+                errold = err;
+                while (abs(err) > 2 && _collector.CommandCode.opModeIsActive()) {
+                    err = distance - _liftM1.getCurrentPosition() / 480 * 360;
+                    double u = (err * kp) + (err - errold) * kd;
+                    _liftM1.setPower(u);
+                    _liftM2.setPower(u);
+                }
+                _liftM1.setPower(0);
+                _liftM2.setPower(0);
     }
 
     private CollectorSample _collector;
@@ -44,7 +45,6 @@ public class Lift {
         _liftM2 = _collector.CommandCode.hardwareMap.get(DcMotor.class, "liftmotor2");
         _liftM1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         _liftM2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         _servoLift1 = _collector.CommandCode.hardwareMap.get(Servo.class, "servoLift1");
         _servoLift2 = _collector.CommandCode.hardwareMap.get(Servo.class, "servoLift2");
 
@@ -55,7 +55,7 @@ public class Lift {
 
         _servoLift1.setPosition(0);
         _servoLift2.setPosition(0);
-    }
+}
 
     public void Update() {
         boolean X = _collector.CommandCode.gamepad1.cross;
@@ -69,7 +69,8 @@ public class Lift {
 
         if (_Lift) {
             moveLift(51);
-        } else {
+        }
+        else {
             moveLift(0);
         }
 
@@ -81,10 +82,10 @@ public class Lift {
 
         if (_Lift1) {
             _servoLift1.setPosition(0);
-        } else {
+        }
+        else {
             _servoLift1.setPosition(0);
         }
-
 
         if (O && !_OOld)
             _Lift2 = !_Lift2;
@@ -93,7 +94,20 @@ public class Lift {
 
         if (_Lift2) {
             _servoLift2.setPosition(0);
-        } else {
+        }
+        else {
+            _servoLift2.setPosition(0);
+        }
+
+        if (O && !_OOld)
+            _Lift2 = !_Lift2;
+
+        _OOld = X;
+
+        if (_Lift2) {
+            _servoLift2.setPosition(0);
+        }
+        else {
             _servoLift2.setPosition(0);
         }
     }
