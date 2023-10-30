98bc65db08300f7b1b079e2c30dcabbd38d8e037
Egor <egorhvog@gmail.com>
2023-10-30 20:37:09 +0300

    add new PIDCmethod

---
+++ Team17517/src/main/java/org/woen/team17517/Robot/PIDCmethod.java
@@ -0,0 +1,47 @@
+package org.woen.team17517.Robot;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Config
+public  class PIDCmethod {
+
+    public PIDCmethod( double kp,double ki, double kd){
+            setCoificent(kp, ki, kd,0);
+    }
+    public PIDCmethod(double kp, double ki, double kd, double ks){
+        setCoificent(kp,ki,kd,ks);
+
+    }
+    double kp,kd,ki,ks = 1;
+    public void setCoificent(double ki,double kp,double kd,double ks){
+
+        this.kp = kp;
+        this.ki = ki;
+        this.kd = kd;
+        this.ks = ks;
+
+    }
+    double time = System.currentTimeMillis();
+    double I,D,P,C,lastWrong = 0;
+    public double PID(double target,double voltage,double enc,double ks) {
+        double time1 = System.currentTimeMillis() / 1000.0-time;
+        double wrong = target - enc;
+        double P = kp * wrong;
+
+        I += wrong * time1 * ki;
+
+        D = ((wrong - lastWrong) / time1) * kd;
+
+        lastWrong = wrong;
+
+        C = 12/voltage;
+
+        double PID = (I + D + P)*C+target*ks;
+
+        return PID;
+
+
+    }
+}
