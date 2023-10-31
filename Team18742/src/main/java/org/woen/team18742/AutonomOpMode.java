70148e5f0b8cb9f6a8e1d0c9a675d4443d9e72e0
dukar <dukarevichroman@gmail.com>
2023-10-31 22:24:03 +0300

    ЛИФТ

--- Team18742/src/main/java/org/woen/team18742/AutonomOpMode.java
+++ Team18742/src/main/java/org/woen/team18742/AutonomOpMode.java
@@ -5,17 +5,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class AutonomOpMode extends LinearOpMode {
-    private AutonomCollector _collector;
+    private TeleOpCollector _collector;
 
     @Override
     public void runOpMode() {
-        _collector = new AutonomCollector(this);
+        _collector = new TeleOpCollector(this);
 
         waitForStart();
         resetRuntime();
 
         _collector.Start();
 
+        _collector.Driver.DriveDirection(0,0,0);
+        ///////////////////////
+        _collector.Driver.DriveDirection(0,1,0);
+        sleep(3000);
+        _collector.Driver.DriveDirection(0,0,0);
+        /////////////////////////
         while (opModeIsActive()) {
             _collector.Update();
         }
