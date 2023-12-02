15f7187ea150ee35428344882cd18820eca16dc5
ZelenkovKonstantin <140378278+ZelenkovKonstantin@users.noreply.github.com>
2023-12-02 20:04:00 +0300

    brush defense

---
+++ Team18742/src/main/java/org/woen/team18742/Tools/ServoControl.java
@@ -0,0 +1,27 @@
+package org.woen.team18742.Tools;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoControl {
+    Servo servo;
+   private double servoRangeDeg = 180;
+    private double servoSekDeg = 180;
+    private double targetDegree = 90;
+    private double nowDegree = 90;
+    public  ServoControl(Servo servo, double sDr,double sdk){
+
+        this.servo = servo;
+        servoRangeDeg = sDr;
+        servoSekDeg = 60 /sdk;
+    }
+    public void setPositionDegres(double deg){
+        deg = deg /servoRangeDeg;
+        servo.setPosition(deg);
+    }
+    public double getTargetDegree(){
+        return targetDegree;
+    }
+    public double getNowDegree(){
+    // nowDegree += zn*sp*time и нужна функция для получ инф достиг ли сервак своей позиции
+    }
+}
