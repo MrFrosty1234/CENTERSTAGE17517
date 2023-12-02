351ca92843fd7015c2c3375bf4d7e94970d8131e
ZelenkovKonstantin <140378278+ZelenkovKonstantin@users.noreply.github.com>
2023-12-02 17:38:56 +0300

    brush defense

--- Team18742/src/main/java/org/woen/team18742/Intake.java
+++ Team18742/src/main/java/org/woen/team18742/Intake.java
@@ -3,22 +3,29 @@ package org.woen.team18742;
 
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 public class Intake {
-    private DcMotor brushMotor; // ц
+    private DcMotorEx brushMotor; // ц
     private Servo gripper; // Штучка которая хватает пиксели в подъемнике
     private Servo clamp; // Сервак который прижимает пиксели после щеток
     private AnalogInput pixelSensor; // Датчик присутствия пикселей над прижимом
     private BaseCollector _collector; // Штука в которой хранится всё остальное
     public static double pixelSensorvoltagekoksik = 1.65;
     boolean inableIntake;
+    private boolean flagdefense = true;
+    ElapsedTime elapsedTime = new ElapsedTime();
 
     public Intake(BaseCollector collector) {
         _collector = collector;
 
-        brushMotor = _collector.CommandCode.hardwareMap.get(DcMotor.class, "brushMotor");
+        brushMotor = _collector.CommandCode.hardwareMap.get(DcMotorEx.class, "brushMotor");
     }
+
     private double speed;
     private double servoGripperreturn = 0.7;
     private double servoGripper = 0.2;
@@ -36,11 +43,54 @@ public class Intake {
     private void intakePower(boolean brush) {
         if (brush) {
             speed = 1;
+
+
             brushMotor.setPower(speed);
             inableIntake = brush;
         } else {
             speed = 0;
             brushMotor.setPower(speed);
+        }
+    }
+
+    private void intakePowerWithDefense(boolean brush1) {//функция для щёток с зашитой от зажёвывания
+        int speed = 100;
+        if (brush1) {
+            if (brushMotor.getCurrent(CurrentUnit.AMPS) <= 0.75 && flagdefense) {
+                elapsedTime.reset();
+            }
+            if (elapsedTime.milliseconds() >= 1.5) {
+                flagdefense = false;
+            }
+            if (elapsedTime.milliseconds() >= 3) {
+                flagdefense = true;
+            }
+            if (!flagdefense) {
+                brushMotor.setPower(-speed);
+            } else {
+                brushMotor.setPower(speed);
+            }
+
+        }
+    }
+    private void intakePowerWithDefense(boolean brush1) {//функция для щёток с зашитой от зажёвывания
+
+        if (brush1) {
+            if (brushMotor.getCurrent(CurrentUnit.AMPS) <= 0.75 && flagdefense) {
+                elapsedTime.reset();
+            }
+            if (elapsedTime.milliseconds() >= 1.5) {
+                flagdefense = false;
+            }
+            if (elapsedTime.milliseconds() >= 3) {
+                flagdefense = true;
+            }
+            if (!flagdefense) {
+                brushMotor.setPower(-speed);
+            } else {
+                brushMotor.setPower(speed);
+            }
+
         }
     }
 
@@ -59,7 +109,7 @@ public class Intake {
         return pixelSensor.getVoltage() > pixelSensorvoltagekoksik;
     }
 
-   public void Update() {
+    public void Update() {
         if (pixelDetected()) {
             setClamp(true);
             if (inableIntake)
