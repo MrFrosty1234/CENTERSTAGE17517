539f21e38b280546aec56b29bf99976bfec61d39
tikhonsmovzh <tikhonsmovzh@gmail.com>
2023-12-06 20:09:14 +0300

    debug lift

--- Team18742/src/main/java/org/woen/team18742/Lift/Lift.java
+++ Team18742/src/main/java/org/woen/team18742/Lift/Lift.java
@@ -7,6 +7,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.woen.team18742.Collectors.BaseCollector;
 import org.woen.team18742.Tools.PID;
+import org.woen.team18742.Tools.ToolTelemetry;
+
+import javax.tools.Tool;
 
 public class Lift {
     private final DcMotor _liftM1;
@@ -16,38 +19,49 @@ public class Lift {
     private double _targetLiftPose = 0;
 
     private final DigitalChannel _ending;
-    private ElapsedTime _time;
 
     public Lift(BaseCollector collector) {
-        _ending = collector.CommandCode.hardwareMap.get(DigitalChannel.class,"ending");
+        _ending = collector.CommandCode.hardwareMap.get(DigitalChannel.class, "ending");
         _liftM1 = collector.CommandCode.hardwareMap.get(DcMotor.class, "liftmotor");
         _liftM1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        _liftPid = new PID(.001, 0, 0, 1);
+        _liftPid = new PID(0.1, 0.04, 0, 1);
 
-      //  _liftM1.setPower(0);
+        _liftM1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        _liftM1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //  _liftM1.setPower(0);
 
         _ending.setMode(DigitalChannel.Mode.INPUT);
-
-
     }
 
     public void Update() {
         boolean state = !_ending.getState();
 
-        if (state) {
+        /*if (state) {
             _liftM1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             _liftM1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
+        }*/
 
-        if (_isZeroing) {
-            if (state || _time.milliseconds() - _zeroingTimeStart < 5000) {
-                _isZeroing = false;
-           //     _liftM1.setPower(0);
-            }
-        } else {
-            // _liftM1.setPower(_liftPid.Update(_targetLiftPose - _liftM1.getCurrentPosition()));
-        }
+        //if (_isZeroing) {
+        // if (state || _time.milliseconds() - _zeroingTimeStart < 5000) {
+        //  _isZeroing = false;
+        //     _liftM1.setPower(0);
+        //}
+        //} else {
+        //if (_liftM1.getCurrentPosition() >= 0)
+            _liftM1.setPower(_liftPid.Update(_targetLiftPose - _liftM1.getCurrentPosition()));
+
+        ToolTelemetry.AddLine(_liftM1.getCurrentPosition() + ", " + _targetLiftPose);
+        //}
+    }
+
+    public void SetTargetPose(double pose) {
+        _targetLiftPose = pose;
+    }
+
+    public double GetLiftPose() {
+        return _targetLiftPose;
     }
 
     public boolean isATarget() {
@@ -67,7 +81,7 @@ public class Lift {
     private long _zeroingTimeStart;
 
     public void Zeroing() {
-      //  _liftM1.setPower(-0.3);
+        //  _liftM1.setPower(-0.3);
 
         _zeroingTimeStart = System.currentTimeMillis();
 
