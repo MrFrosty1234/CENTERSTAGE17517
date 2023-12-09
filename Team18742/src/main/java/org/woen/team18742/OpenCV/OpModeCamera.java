61be0a37f10c9d592740dff54396011d9ba36de4
ZelenkovKonstantin <140378278+ZelenkovKonstantin@users.noreply.github.com>
2023-12-09 22:06:24 +0300

    camerA

--- Team18742/src/main/java/org/woen/team18742/OpenCV/OpModeCamera.java
+++ Team18742/src/main/java/org/woen/team18742/OpenCV/OpModeCamera.java
@@ -5,21 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagMetadata;
-import org.firstinspires.ftc.vision.apriltag.AprilTagPoseRaw;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @TeleOp(name = "Test Pipeline OpMode")
 public class OpModeCamera extends LinearOpMode {
     VisionPortal visionPortal;
-    PipeLine pipeLine = new PipeLine();
+
+
+    VisionPipeLine pipeLine = new VisionPipeLine();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -38,6 +31,7 @@ public class OpModeCamera extends LinearOpMode {
             telemetry.addData("number of parking", pipeLine.pos);
             telemetry.addData("team", pipeLine.team);
             telemetry.addData("camera", visionPortal.getCameraState());
+           // telemetry.addData("hcv:", visionPortal.9)
             telemetry.update();
         }
 
--- Team18742/src/main/java/org/woen/team18742/OpenCV/VisionPipeLine.java
+++ Team18742/src/main/java/org/woen/team18742/OpenCV/VisionPipeLine.java
@@ -14,7 +14,7 @@ import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 
-class PipeLine implements VisionProcessor {
+class VisionPipeLine implements VisionProcessor {
     double x = 640;
     double y = 480;
     double r1 = 5;
@@ -25,8 +25,8 @@ class PipeLine implements VisionProcessor {
     double b2 = 296;
     Mat img_range_red = new Mat();
     Mat img_range_blue = new Mat();
-    public double hRedDown = 10;
-    public double cRedDown = 50;
+    public double hRedDown = 4;
+    public double cRedDown = 127.7;
     public double vRedDowm = 154.4;
     public double hRedUp = 30;
     public double cRedUp = 255;
@@ -57,6 +57,7 @@ class PipeLine implements VisionProcessor {
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
         //
+
         cvtColor(frame, frame, COLOR_RGB2HSV);//конвертация в хсв
         resize(frame, frame, new Size(x, y));// установка разрешения
 
@@ -65,12 +66,12 @@ class PipeLine implements VisionProcessor {
             inRange(frame, new Scalar(hRedDown, cRedDown, vRedDowm), new Scalar(hRedUp, cRedUp, vRedUp), img_range_red);
 
             //inRange(картинка вход, мин знач хсв, макс знач хсв, выход картинка(трешхолды))
-            //inRange(frame, new Scalar(hBlueDown, cBlueDown, vBlueDowm), new Scalar(hBlueUp, cBlueUp, vBlueUp), img_range_blue);
+            inRange(frame, new Scalar(hBlueDown, cBlueDown, vBlueDowm), new Scalar(hBlueUp, cBlueUp, vBlueUp), img_range_blue);
 
 
-        Core.bitwise_or(img_range_red, img_range_red, frame);//объединяем два инрейнджа
+        Core.bitwise_or(img_range_blue, img_range_blue, frame);//объединяем два инрейнджа
 
-        Rect boundingRect = boundingRect(img_range_red);//boudingRect рисуем прямоугольник
+        Rect boundingRect = boundingRect(frame);//boudingRect рисуем прямоугольник
 
         centerOfRectX = boundingRect.x + boundingRect.width / 2.0;//координаты центра вычисляем
         centerOfRectY = boundingRect.y + boundingRect.height / 2.0;
