353f0f51937bfd52c3d04cfdf2de1ad3ee5dd061
tikhonsmovzh <tikhonsmovzh@gmail.com>
2023-12-10 13:55:11 +0300

    fix camera3

--- Team18742/src/main/java/org/woen/team18742/Modules/Camera/Camera.java
+++ Team18742/src/main/java/org/woen/team18742/Modules/Camera/Camera.java
@@ -2,20 +2,22 @@ package org.woen.team18742.Modules.Camera;
 
 import com.acmerobotics.dashboard.config.Config;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.woen.team18742.Collectors.BaseCollector;
 import org.woen.team18742.OpenCV.PipeLine;
+import org.woen.team18742.OpenCV.TestVisionProcessor;
 import org.woen.team18742.Tools.ToolTelemetry;
 
 @Config
 public class Camera {
-    private final WebcamName _camera;
+    private CameraName _camera;
     public static boolean IsDebug = false;
     public static int RobotPos;
 
     VisionPortal visionPortal;
-    PipeLine pipeLine = new PipeLine();
+    TestVisionProcessor pipeLine = new TestVisionProcessor();
 
     public Camera(BaseCollector collector) {
         _camera = collector.CommandCode.hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -25,7 +27,7 @@ public class Camera {
                 .addProcessor(pipeLine)
                 .build();
 
-        visionPortal.setProcessorEnabled(pipeLine, false);
+       // visionPortal.setProcessorEnabled(pipeLine, false);
     }
 
     private int Pose = 0;
@@ -49,7 +51,7 @@ public class Camera {
     }
 
     public void Start() {
-        visionPortal.setProcessorEnabled(pipeLine, true);
+        //  visionPortal.setProcessorEnabled(pipeLine, true);
         Pose = pipeLine.pos;
         ToolTelemetry.AddLine("camera = " + Pose);
     }
