ef2f5e3109f1f6cfa6fb69ec77c1f93ae24ac95b
Aleksandr Medvedev <medvedev.asm@outlook.com>
2024-01-03 22:29:12 +0300

    17517 not left behind

---
+++ MeepMeep17517/src/main/java/org/woen/meepmeep17517/MeepMeep17517.java
@@ -0,0 +1,40 @@
+package org.woen.meepmeep17517;
+
+import static java.lang.Math.PI;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeep17517 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        double maxVel = 200d;
+        double maxAccel = 200d;
+        double maxAngVel = Math.toRadians(270d);
+        double maxAngAccel = Math.toRadians(270d);
+        double trackWidth = 35d;
+        final double INCH_TO_CM = 1d / 2.54d;
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(maxVel * INCH_TO_CM, maxAccel * INCH_TO_CM, maxAngVel, maxAngAccel, trackWidth * INCH_TO_CM)
+                .build();
+
+        myBot.runAction(myBot.getDrive()
+                .actionBuilder(new Pose2d(-150*INCH_TO_CM, 150*INCH_TO_CM, 0))
+                .splineTo(new Vector2d(100 * INCH_TO_CM, 30 * INCH_TO_CM), 0)
+                .lineToX(30 * INCH_TO_CM)
+                .turn(PI / 2)
+                .lineToY(-30 * INCH_TO_CM)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
