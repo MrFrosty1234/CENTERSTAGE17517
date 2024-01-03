f5f6072b4d5c1392f8ae4abaafb213afe7cd1b56
Aleksandr Medvedev <medvedev.asm@outlook.com>
2024-01-03 22:02:09 +0300

    Upgrade MeepMeep to rr1.0

--- MeepMeep18742/src/main/java/org/woen/meepmeep18742/MeepMeep18742.java
+++ MeepMeep18742/src/main/java/org/woen/meepmeep18742/MeepMeep18742.java
@@ -1,12 +1,14 @@
-package org.woen.meepmeeptesting;
+package org.woen.meepmeep18742;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
+import static java.lang.Math.PI;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class MeepMeep18742 {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
         double maxVel = 150d;
@@ -14,15 +16,19 @@ public class MeepMeepTesting {
         double maxAngVel = Math.toRadians(180d);
         double maxAngAccel = Math.toRadians(180d);
         double trackWidth = 35d;
-        final double INCH_TO_CM = 1d/2.54d;
+        final double INCH_TO_CM = 1d / 2.54d;
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(maxVel*INCH_TO_CM, maxAccel*INCH_TO_CM, Math.toRadians(180), Math.toRadians(180), trackWidth*INCH_TO_CM)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .splineTo(new Vector2d(30,30),0)
-                                .build()
-                );
+                .setConstraints(maxVel * INCH_TO_CM, maxAccel * INCH_TO_CM, Math.toRadians(180), Math.toRadians(180), trackWidth * INCH_TO_CM)
+                .build();
+
+        myBot.runAction(myBot.getDrive()
+                .actionBuilder(new Pose2d(0, 0, 0))
+                .splineTo(new Vector2d(100 * INCH_TO_CM, 30 * INCH_TO_CM), 0)
+                .lineToX(30 * INCH_TO_CM)
+                .turn(PI / 2)
+                .lineToY(-30 * INCH_TO_CM)
+                .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
