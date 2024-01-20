945dd6d10a9e956ae8588e5b56e6199e321b0b5f
dukar <dukarevichroman@gmail.com>
2024-01-20 17:43:24 +0300

    тихон не лох а красавчик

---
+++ Team18742/src/main/java/org/woen/team18742/Modules/Odometry/Podtyaga.java
@@ -0,0 +1,20 @@
+package org.woen.team18742.Modules.Odometry;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.woen.team18742.Collectors.BaseCollector;
+import org.woen.team18742.Modules.Manager.IRobotModule;
+import org.woen.team18742.Modules.Manager.Module;
+import org.woen.team18742.Tools.Devices;
+
+@Module
+public class Podtyaga implements IRobotModule {
+    @Override
+    public void Init(BaseCollector collector) {
+
+    }
+
+    public void otstrel_servacov() {
+
+    }
+}
