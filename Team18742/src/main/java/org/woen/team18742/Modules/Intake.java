086abc9b1847773192d70cb3a14115fdc10c61c2
Aleksandr Medvedev <medvedev.asm@outlook.com>
2024-01-30 02:05:46 +0300

    Changed pixel sensor from 2 to 1 (and there now is only 1 of them)

--- Team18742/src/main/java/org/woen/team18742/Modules/Intake.java
+++ Team18742/src/main/java/org/woen/team18742/Modules/Intake.java
@@ -87,7 +87,7 @@ private Brush _brush;
     ElapsedTime pixelTimer = new ElapsedTime();
 
     private boolean isPixelDetected() {
-        if (pixelSensor2.getVoltage() >= Configs.Intake.pixelSensorvoltage || !_brush.isBrusnOn()/*&& pixelSensor2.getVoltage() >= pixelSensorvoltage*/)
+        if (pixelSensor1.getVoltage() >= Configs.Intake.pixelSensorvoltage || !_brush.isBrusnOn()/*&& pixelSensor2.getVoltage() >= pixelSensorvoltage*/)
             pixelTimer.reset();
         return pixelTimer.milliseconds() > Configs.Intake.pixelDetectTimeMs;
     }
