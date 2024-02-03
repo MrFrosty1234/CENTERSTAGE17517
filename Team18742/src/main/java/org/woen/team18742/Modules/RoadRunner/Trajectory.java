82e731081bc38416cc931de56d1a8dc3093bee53
ZelenkovKonstantin <Konstantinzelenkov9@gmail.com>
2024-02-03 10:47:54 +0300

    roadrunner meepmeep shechematik

--- Team18742/src/main/java/org/woen/team18742/Modules/RoadRunner/Trajectory.java
+++ Team18742/src/main/java/org/woen/team18742/Modules/RoadRunner/Trajectory.java
@@ -9,7 +9,7 @@ import org.woen.team18742.Tools.Bios;
 
 public class Trajectory {
     public static RoadRunnerRouteManager.MyTrajectoryBuilder GetTrajectory(RoadRunnerRouteManager.MyTrajectoryBuilder builder, CameraRobotPosition camera) {
-        switch (camera) {
+       /* switch (camera) {
             case FORWARD:
                 builder= builder
                         .lineToY(110 )
@@ -79,6 +79,28 @@ public class Trajectory {
                         .strafeToLinearHeading(new Vector2d(122 / 2.54, 100 / 2.54), 0);
             break;
 
-        }return builder;
+        }*/
+        builder = builder
+                .lineToY(110 / 2.54)
+                //.splineTo(new Vector2d(25.4 / 2.54, 85 / 2.54), -PI / 2)//forward
+                .splineTo(new Vector2d(25 / 2.54, 90 / 2.54), -PI) //left
+                .lineToX(30 / 2.54)//left
+                //.splineTo(new Vector2d(35 / 2.54, 90 / 2.54), 0) //right
+                //.strafeToLinearHeading(new Vector2d(18 / 2.54, 85 / 2.54), 0)//right
+                //пиксель на линию
+                .strafeToLinearHeading(new Vector2d(60 / 2.54, 150 / 2.54), 0)//universal
+                //.strafeToLinearHeading(new Vector2d(120 / 2.54, 75 / 2.54), 0)//right
+                //.strafeToLinearHeading(new Vector2d(123 / 2.54, 90 / 2.54), 0)//forward
+                .strafeToLinearHeading(new Vector2d(123 / 2.54, 105 / 2.54), 0)//left
+                //тут ставим пиксель на задник
+                .strafeToLinearHeading(new Vector2d(70 / 2.54, 30 / 2.54), 0)
+                .strafeToLinearHeading(new Vector2d(-80 / 2.54, 30 / 2.54), 0)
+                //врубили щётки
+                //начали опускать
+                .strafeToLinearHeading(new Vector2d(-145 / 2.54, 30 / 2.54), 0)
+                //хаваем стопки
+                .strafeToLinearHeading(new Vector2d(120 / 2.54, 30 / 2.54), 0)
+                .strafeToLinearHeading(new Vector2d(122 / 2.54, 100 / 2.54), 0);
+        return builder;
     }
 }
