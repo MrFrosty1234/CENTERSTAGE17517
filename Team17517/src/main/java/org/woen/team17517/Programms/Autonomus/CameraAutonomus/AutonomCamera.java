104daa67a357513005d0d2284b003789b6c42270
egorkhvostikove <egorhvog@gmail.com>
2024-02-07 19:23:32 +0300

    For MAksim

--- Team17517/src/main/java/org/woen/team17517/Programms/Autonomus/CameraAutonomus/AutonomCamera.java
+++ Team17517/src/main/java/org/woen/team17517/Programms/Autonomus/CameraAutonomus/AutonomCamera.java
@@ -2,15 +2,81 @@ package org.woen.team17517.Programms.Autonomus.CameraAutonomus;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.woen.team17517.Programms.Autonomus.OldAutonomus.AutonomBaseClass;
+import org.woen.team17517.RobotModules.OpenCV.PipeLine;
 import org.woen.team17517.RobotModules.UltRobot;
+import org.woen.team17517.Service.Button;
 
 @Autonomous
 public class AutonomCamera extends LinearOpMode {
     UltRobot robot;
+    boolean dpadUp = false;
+    boolean dpadDown = false;
+    boolean dpadRight = false;
+    Button dpadRightButton = new Button();
+    boolean dpadLeft = false;
+    Button dpadLeftButton = new Button();
+    Button dpadDownButton = new Button();
+    Button dpadUpButton = new Button();
+    boolean rightBumper = false;
+    Button rightBumperButton   = new Button();
+    boolean rightTriger = false;
+    Button rigtTrigerButton = new Button();
+    StartTeam startTeam = StartTeam.BlUE;
+    StartPosition startPosition = StartPosition.RIGHT;
+    @Override
+    public void waitForStart() {
+        long  timeToSleep= 0;
+        while (!isStarted()) {
+            dpadDown = gamepad1.dpad_down;
+            dpadUp = gamepad1.dpad_up;
+            dpadLeft = gamepad1.dpad_left;
+            dpadRight = gamepad1.dpad_right;
+            rightBumper = gamepad1.right_bumper;
+            rightTriger = gamepad1.right_trigger > 0;
+
+            if (dpadDownButton.update(dpadDown)){
+                startTeam = StartTeam.BlUE;
+            }
+            if (dpadUpButton.update(dpadUp)){
+                startTeam = StartTeam.RED;
+            }
+            if (dpadLeftButton.update(dpadLeft)){
+                startPosition = StartPosition.LEFT;
+            }
+            if (dpadRightButton.update(dpadRight)){
+                startPosition = StartPosition.RIGHT;
+            }
+            if (rightBumperButton.update(rightBumper)){
+                timeToSleep += 1000;
+            }
+            if (rigtTrigerButton.update(rightTriger)){
+                timeToSleep += 5000;
+            }
+
+            telemetry.addData("Team",startTeam);
+            telemetry.addData("Position",startPosition);
+            telemetry.addData("Time to sleep", timeToSleep);
+            telemetry.update();
+        }
+        sleep(timeToSleep);
+    }
+
+
+    enum StartTeam{
+        BlUE,
+        RED
+    }
+    enum StartPosition{
+        RIGHT,
+        LEFT
+    }
+
     public void runOpMode() {
         robot = new UltRobot(this);
         waitForStart();
-        ElementPosition elementPos = ElementPosition.FRONT;
+        ElementPosition elementPos = ElementPosition.FRONT;///HERE
         switch (elementPos){
             case FRONT:
                 robot.autnomModules.Move(0,24000,0,1);
@@ -25,5 +91,9 @@ public class AutonomCamera extends LinearOpMode {
                 robot.autnomModules.Move(0,-24000,4000,0);
                 break;
         }
+        switch (startTeam){
+            case RED:
+        }
+
     }
 }
