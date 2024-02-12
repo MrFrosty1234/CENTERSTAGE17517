ededb7bde0d7e9bd753ce76ab45fa6eeaaacfb95
EgorKhvostikov <egorhvog@gmail.com>
2024-02-12 22:23:02 +0300

    Refactor

--- Team17517/src/main/java/org/woen/team17517/RobotModules/Grabber/GrabberOpenClosePosition.java
+++ Team17517/src/main/java/org/woen/team17517/RobotModules/Grabber/GrabberOpenClosePosition.java
@@ -1,6 +1,6 @@
 package org.woen.team17517.RobotModules.Grabber;
 
-public enum GrabberOpenClosePosition {
+public enum GrabberOpenClosePosition {//test
     OPEN(0.565),CLOSE(0.465);
     double value;
     GrabberOpenClosePosition(double value){this.value = value;}
