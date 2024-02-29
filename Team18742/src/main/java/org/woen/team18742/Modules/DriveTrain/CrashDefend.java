6572c8de4e0cc7f71cad9f27937baeaa9411ce6c
tikhonsmovzh <tikhonsmovzh@gmail.com>
2024-02-29 17:59:36 +0300

    CrashDefend contains

--- Team18742/src/main/java/org/woen/team18742/Modules/DriveTrain/CrashDefend.java
+++ Team18742/src/main/java/org/woen/team18742/Modules/DriveTrain/CrashDefend.java
@@ -1,5 +1,7 @@
 package org.woen.team18742.Modules.DriveTrain;
 
+import com.acmerobotics.roadrunner.Vector2d;
+
 import org.woen.team18742.Collectors.BaseCollector;
 import org.woen.team18742.Modules.Gyroscope;
 import org.woen.team18742.Modules.Manager.IRobotModule;
@@ -16,7 +18,9 @@ public class CrashDefend implements IRobotModule {
     private Gyroscope _gyro;
     private Drivetrain _driveTrain;
 
-    private Square _square;
+    private Square[] _zones = new Square[]{
+            new Square(Vector2.ZERO, new Vector2(50, 50))
+    };
 
     @Override
     public void Init(BaseCollector collector) {
@@ -27,7 +31,7 @@ public class CrashDefend implements IRobotModule {
 
     @Override
     public void Update() {
-        _square = new Square(
+        Square _robot = new Square(
                 Vector2.Plus(_odometry.Position,
                         new Vector2(Configs.DriveTrainWheels.WheelsRadius, Configs.DriveTrainWheels.WheelsRadius).Turn(_gyro.GetRadians())),
                 Vector2.Plus(_odometry.Position,
@@ -38,19 +42,51 @@ public class CrashDefend implements IRobotModule {
                         new Vector2(-Configs.DriveTrainWheels.WheelsRadius, Configs.DriveTrainWheels.WheelsRadius).Turn(_gyro.GetRadians()))
         );
 
-        Triangle[] triangles = _square.GetTriangles();
+        ToolTelemetry.DrawPolygon(_robot.GetPoints(), Color.BLUE);
 
-
-        ToolTelemetry.DrawPolygon(_square.GetPoints(), Color.BLUE);
+        for (Square i : _zones) {
+            if(IsContains(i, _robot))
+                ToolTelemetry.DrawPolygon(i.GetPoints(), Color.RED);
+            else
+                ToolTelemetry.DrawPolygon(i.GetPoints(), Color.ORANGE);
+        }
     }
 
-    private class Triangle {
-        public Vector2 p1, p2, p3;
+    private static boolean IsContains(Square s1, Square s2) {
+        for (Line i : s1.GetLines()) {
+            for (Line j : s2.GetLines()) {
+                if(i.isContains(j))
+                    return true;
+            }
+        }
 
-        public Triangle(Vector2 p1, Vector2 p2, Vector2 p3) {
+        return false;
+    }
+
+    private class Line {
+        public Vector2 p1, p2;
+
+        public Line(Vector2 p1, Vector2 p2) {
             this.p1 = p1;
             this.p2 = p2;
-            this.p3 = p3;
+        }
+
+        public boolean isContains(Line l) {
+            double parallelism = (p1.X - p2.X) * (l.p1.Y - l.p2.Y) - (p1.Y - p2.Y) * (l.p1.X - l.p2.X);
+
+            if (Math.abs(parallelism) < 0.01d)
+                return false;
+
+            Vector2 point = new Vector2(((p1.X * p2.Y - p1.Y * p2.X) * (l.p1.X - l.p2.X) - (p1.X - p2.X) * (l.p1.X * l.p2.Y - l.p1.Y * l.p2.X)) / parallelism,
+                    ((p1.X * p2.Y - p1.Y * p2.X) * (l.p1.Y - l.p2.Y) - (p1.Y - p2.Y) * (l.p1.X * l.p2.Y - l.p1.Y * l.p2.Y)) / parallelism);
+
+            if (p1.X <= point.X && point.X <= p2.X && p1.Y <= point.Y && point.Y <= p2.Y)
+                return true;
+
+            if (l.p1.X <= point.X && point.X <= l.p2.X && l.p1.Y <= point.Y && point.Y <= l.p2.Y)
+                return true;
+
+            return false;
         }
     }
 
@@ -64,6 +100,13 @@ public class CrashDefend implements IRobotModule {
             this.p4 = p4;
         }
 
+        public Square(Vector2 pos, Vector2 size) {
+            p1 = Vector2.Plus(pos, Vector2.Multiply(size, new Vector2(0.5, 0.5)));
+            p2 = Vector2.Plus(pos, Vector2.Multiply(size, new Vector2(0.5, -0.5)));
+            p3 = Vector2.Plus(pos, Vector2.Multiply(size, new Vector2(-0.5, -0.5)));
+            p4 = Vector2.Plus(pos, Vector2.Multiply(size, new Vector2(-0.5, 0.5)));
+        }
+
         public Vector2[] GetPoints() {
             return new Vector2[]{
                     p1,
@@ -73,10 +116,12 @@ public class CrashDefend implements IRobotModule {
             };
         }
 
-        public Triangle[] GetTriangles() {
-            return new Triangle[]{
-                    new Triangle(_square.p1, _square.p2, _square.p3),
-                    new Triangle(_square.p3, _square.p4, _square.p1)
+        public Line[] GetLines() {
+            return new Line[]{
+                    new Line(p1, p2),
+                    new Line(p2, p3),
+                    new Line(p3, p4),
+                    new Line(p4, p1)
             };
         }
     }
