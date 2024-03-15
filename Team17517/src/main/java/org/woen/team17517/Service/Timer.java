43b9787ed804c1bb59d4f2f47316949231b84379
MrFrosty1234 <mitrusinmaksim@gmail.com>
2024-03-16 00:27:34 +0300

    autonom 2+4 traectory + timer. ty,oaleksander and tihon

--- Team17517/src/main/java/org/woen/team17517/Service/Timer.java
+++ Team17517/src/main/java/org/woen/team17517/Service/Timer.java
@@ -1,6 +1,14 @@
 package org.woen.team17517.Service;
 
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.woen.team17517.R;
 import org.woen.team17517.RobotModules.UltRobot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BooleanSupplier;
 
 public class Timer implements RobotModule {
     double timeForTimer = 0;
@@ -8,6 +16,10 @@ public class Timer implements RobotModule {
     double startTime = 0;
     double currentMatchTime = 0;
     UltRobot robot;
+
+    private double startTimeForRR;
+    private final ElapsedTime time = new ElapsedTime();
+    private List<BooleanSupplier> wait = new ArrayList<>();
 
     public Timer(UltRobot robot) {
         this.robot = robot;
@@ -21,6 +33,15 @@ public class Timer implements RobotModule {
     public void getTimeForTimer(double timeTarget) {
         startTime = System.currentTimeMillis();
         this.timeForTimer = timeTarget;
+    }
+
+    public void waitLift(){
+       robot.updateWhilePositionFalse(new Runnable[]{
+               () -> {
+                   wait.add(()-> robot.lift.isAtPosition() && robot.intake.isAtPosition());
+                   startTimeForRR = time.seconds();
+               }
+       });
     }
 
     @Override
